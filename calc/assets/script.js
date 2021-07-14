77eb9c2c2c3d79180ce308a4186278ed6dc24d66
cryptocalc <cryptocalc@yandex.by>
2021-07-14 03:06:27 +0300

    feat/add calculation

--- calc/assets/script.js
+++ calc/assets/script.js
@@ -1,22 +1,65 @@
 'use strict'
 
-let koef;
+const formatNumberString = (strNumber = '') => {
+    if (strNumber === null) {
+        return '';
+    }
 
-fetch('https://api.cryptowat.ch/markets/huobi/btchusd/price?apikey=9HZJQFW30CBDQYXPDHIJ')
-.then((data) => data.json())
-.then(({ data }) => console.log(data));
+    const parts = strNumber.split('.');
+
+    parts[0] = parts[0]
+        .split('')
+        .reverse()
+        .join('')
+        .replace(/\d{3}(?=.)/g, '$&,')
+        .split('')
+        .reverse()
+        .join('');
+
+    return parts.join('.');
+};
+
+const clearNumberString = (strNumber = '') => strNumber.replace(/[^0-9.]/g, '');
+
+const createHandler = (input, resultCallback) => {
+    let value = input.value || '';
+    return (
+        (event) => {
+            const clearText = clearNumberString(event.target.value);
+            if (/^(\d+\.?\d{0,8})?$/.test(clearText)) {
+                value = clearText;
+                resultCallback(Number(clearText))
+                input.value = formatNumberString(clearText);
+            } else {
+                input.value = formatNumberString(value);
+            }
+        }
+    )
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+    let price;
     const payInput = document.getElementById('calc-pay');
     const getInput = document.getElementById('calc-get');
 
-    payInput.addEventListener('input', (e) => {
-        getInput.value = e.target.value;
-    })
+    fetch('/api-calc')
+    .then((res) => res.json())
+    .then((data) => { 
+        price = data.price;
 
-    getInput.addEventListener('input', (e) => {
-        pauInput.value = e.target.value;
-    })
+        const initialValue = Number(clearNumberString(payInput.value));
+        getInput.value = formatNumberString((initialValue / price).toFixed(8)).replace(/[\.0]*$/g, '');
 
+        payInput.addEventListener('input', createHandler(payInput, (value) => {
+            getInput.value = formatNumberString((value / price).toFixed(8)).replace(/[\.0]*$/g, '');
+        }));
+    
+        getInput.addEventListener('input', createHandler(getInput, (value) => {
+            payInput.value = formatNumberString((value * price).toFixed(8)).replace(/[\.0]*$/g, '');
+        }));
+
+        payInput.removeAttribute('readonly');
+        getInput.removeAttribute('readonly');
+    });
 })
 
